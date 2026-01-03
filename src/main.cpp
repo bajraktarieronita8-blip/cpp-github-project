ced29bb2ebbeb0052935746532decd3f083c02cd
bajraktarieronita8-blip <bajraktarieronita8@gmail.com>
2026-01-03 21:58:44 +0100

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,1 +1,22 @@
-cout << "Project with branches and PRs!" << endl;
+#include <iostream>
+using namespace std;
+
+int main() {
+    int num;
+    cout << "Shkruaj nje numer pozitiv: ";
+    cin >> num;
+
+    if (num <= 0) {
+        cout << "Numri nuk eshte pozitiv!" << endl;
+    } else {
+        cout << "Numrat nga 1 deri ne " << num << " jane:" << endl;
+        int i = 1;
+        while (i <= num) {
+            cout << i << " ";
+            i++;
+        }
+        cout << endl; // shton një linjë të re në fund
+    }
+
+    return 0;
+}
